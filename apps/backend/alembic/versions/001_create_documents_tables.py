7e4f9aacf301d9fe5471af814a360e6942dc4ad6
sajidali-10 <sajidali_10@yahoo.com>
2026-06-19 19:01:14 +0000

    fix(db): repair alembic base revision

--- apps/backend/alembic/versions/001_create_documents_tables.py
+++ apps/backend/alembic/versions/001_create_documents_tables.py
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = '001_create_documents_tables'
+revision: str = '001'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
